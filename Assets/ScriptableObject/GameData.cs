433708b0d97a401e94531b422075fe1d477d3cf9
Dionathan00 <111833679+Dionathann@users.noreply.github.com>
2024-09-05 19:44:10 +0700

    demo scriptable Object
    set new scriptable Object

---
+++ Assets/ScriptableObject/GameData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName =("Create New Data"))]
+public class GameData : ScriptableObject
+{
+    public int nomor;
+    public string nama;
+    public Sprite Gambar;
+}
